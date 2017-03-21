8f5683f8c6eb2ee457ca951c64d5b9e92ec978e8
mohohohoroho <michamatysek@gmail.com>
2017-03-21 20:05:48 +0100

    SaveToFile

--- Projekt1/AlgorithmsSearch/LinearSearch.cs
+++ Projekt1/AlgorithmsSearch/LinearSearch.cs
@@ -6,19 +6,12 @@ namespace Projekt1.AlgorithmsSearch
     {
         private int[] vector;
         private int numberToFind;
-        private int _arrayLengthToMeassure;
         private int _attempts = 100000000;
 
         public LinearSearch(int[] vector, int numberToFind)
         {
             this.vector = vector;
             this.numberToFind = numberToFind;
-            _arrayLengthToMeassure = vector.Length;
-        }
-
-        public void SetLimitArrayToCheck(int limit)
-        {
-            _arrayLengthToMeassure = limit < vector.Length ? limit : vector.Length;
         }
 
         public void SetAttempts(int limit)
@@ -43,24 +36,13 @@ namespace Projekt1.AlgorithmsSearch
             return (stopTime - startTime) / (double)Stopwatch.Frequency;
         }
 
-        public int LinearSearchAlgorithm()
+        public bool LinearSearchAlgorithm()
         {
-            int left = 0;
-            int right = _arrayLengthToMeassure - 1;
+            foreach (int t in vector)
+                if (t == numberToFind)
+                    return true;
 
-            while(left <= right)
-            {
-                int middle = (left + right) >> 1;
-
-                if (vector[middle] == numberToFind)
-                    return middle;
-                if (vector[middle] > numberToFind)
-                    right = middle - 1;
-                else
-                    left = middle + 1;
-            }
-
-            return -1;
+            return false;
         }
     }
 }
